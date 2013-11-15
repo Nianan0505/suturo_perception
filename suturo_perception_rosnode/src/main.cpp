6d065772d7633633f1375d2c1dbc67fa75426ba5
Andre Lübken <aluebken@tzi.de>
2013-11-15 14:13:51 +0100

    indent + fixes to rosnode

--- suturo_perception_rosnode/src/main.cpp
+++ suturo_perception_rosnode/src/main.cpp
@@ -8,87 +8,96 @@
 class SuturoPerceptionROSNode
 {
 public:
-    /*
-     * Constructor
-     */
-    SuturoPerceptionROSNode(ros::NodeHandle& n) : nh(n)
+  /*
+   * Constructor
+   */
+  SuturoPerceptionROSNode(ros::NodeHandle& n) : nh(n)
+  {
+    clusterService = nh.advertiseService("GetClusters", 
+      &SuturoPerceptionROSNode::getClusters, this);
+    objectID = 0;
+    //processing = false;
+  }
+
+  /*
+   * Receive callback for the /camera/depth_registered/points subscription
+   */
+   void receive_cloud(const sensor_msgs::PointCloud2ConstPtr& inputCloud)
+   {
+    // process only one cloud
+    ROS_INFO("Receiving cloud");
+    if(processing)
     {
-        processing = false;
+      ROS_INFO("processing...");
+      pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZRGB>());
+      pcl::fromROSMsg(*inputCloud,*cloud_in);
+
+      sp.process_cloud(cloud_in);
+      processing = false;
+
+      ROS_INFO("Received a new point cloud: size = %lu",cloud_in->points.size());
+    }
+  }
+
+  /*
+   * Implementation of the GetClusters Service.
+   *
+   * This method will subscribe to the /camera/depth_registered/points topic, 
+   * wait for the processing of a single point cloud, and return the result from
+   * the calulations as a list of PerceivedObjects.
+   */
+  bool getClusters(suturo_perception_msgs::GetClusters::Request &req,
+    suturo_perception_msgs::GetClusters::Response &res)
+  {
+    ros::Subscriber sub;
+    processing = true;
+
+    // signal failed call, if request string does not match
+    if (req.s.compare("get") != 0)
+    {
+      return false;
     }
 
-    /*
-     * Receive callback for the /camera/depth_registered/points subscription
-     */
-    void receive_cloud(const sensor_msgs::PointCloud2ConstPtr& inputCloud)
+    // Subscribe to the depth information topic
+    sub = nh.subscribe("/camera/depth_registered/points", 1, 
+      &SuturoPerceptionROSNode::receive_cloud, this);
+
+    ROS_INFO("Waiting for processed cloud");
+    ros::Rate r(20); // 20 hz
+    while(processing)
     {
-        // process only one cloud
-        if(processing)
-        {
-            pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZRGB>());
-            pcl::fromROSMsg(*inputCloud,*cloud_in);
-
-            sp.process_cloud(cloud_in);
-            processing = false;
-
-            ROS_INFO("Received a new point cloud: size = %lu",cloud_in->points.size());
-        }
+      ros::spinOnce();
+      r.sleep();
     }
+    ROS_INFO("Cloud processed. Lock buffer and return the results");
 
-    /*
-     * Implementation of the GetClusters Service.
-     *
-     * This method will subscribe to the /camera/depth_registered/points topic, 
-     * wait for the processing of a single point cloud, and return the result from
-     * the calulations as a list of PerceivedObjects.
-     */
-    bool getClusters(suturo_perception_msgs::GetClusters::Request &req,
-                     suturo_perception_msgs::GetClusters::Response &res)
-    {
-        ros::Subscriber sub;
-        processing = true;
+    mutex.lock();
+    res.perceivedObjs = perceivedObjects;
+    mutex.unlock();
 
-        // signal failed call, if request string does not match
-        if (req.s.compare("get") != 0)
-        {
-          return false;
-        }
-
-        // Subscribe to the depth information topic
-        sub = nh.subscribe("/camera/depth_registered/points", 1, 
-          &SuturoPerceptionROSNode::receive_cloud, this);
-
-        ROS_INFO("Waiting for processed cloud");
-            ros::Rate r(20); // 20 hz
-        while(processing){
-                ros::spinOnce();
-                r.sleep();
-            }
-        ROS_INFO("Cloud processed. Lock buffer and return the results");
-
-        mutex.lock();
-        res.perceivedObjs = perceivedObjects;
-        mutex.unlock();
-
-        return true;
-    }
+    return true;
+  }
 
 private:
-    bool processing; // processing flag
-    SuturoPerception sp;
-    std::vector<suturo_perception_msgs::PerceivedObject> perceivedObjects;
-    ros::NodeHandle nh;
-    boost::signals2::mutex mutex;   
+  bool processing; // processing flag
+  SuturoPerception sp;
+  std::vector<suturo_perception_msgs::PerceivedObject> perceivedObjects;
+  ros::NodeHandle nh;
+  boost::signals2::mutex mutex;
+  // ID counter for the perceived objects
+  int objectID;
+  ros::ServiceServer clusterService;  
 };
 
 
 int main (int argc, char** argv)
 {
-    ros::init(argc, argv, "suturo_perception_rosnode");
-    ros::NodeHandle nh;
-    SuturoPerceptionROSNode spr(nh);
-    ROS_INFO("suturo_perception READY");
+  ros::init(argc, argv, "suturo_perception");
+  ros::NodeHandle nh;
+  SuturoPerceptionROSNode spr(nh);
+  ROS_INFO("suturo_perception READY");
     //sp.sayHi();
-    ros::MultiThreadedSpinner spinner(2);
-    spinner.spin();
-    return (0);
+  ros::MultiThreadedSpinner spinner(2);
+  spinner.spin();
+  return (0);
 }
