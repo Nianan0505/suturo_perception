00b010337534f2ed4aa89c69860b00ef5a978276
Andre Lübken <aluebken@tzi.de>
2013-11-15 15:48:49 +0100

    add test client for PerceivedObject Service

---
+++ suturo_perception_rosnode/src/client.cpp
@@ -0,0 +1,59 @@
+/**
+ * This class implements the client interface to query
+ * the PerceivedObjects from the PerceptionServer.
+ *
+ * It will execute a service call to "GetClusters" and print
+ * the returned list of PerceivedObjects in a textual form.
+ */
+#include "ros/ros.h"
+#include "suturo_perception_msgs/GetClusters.h"
+#include <cstdlib>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp> 
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "perception_client");
+
+  ros::NodeHandle n;
+
+  ros::ServiceClient clusterClient = n.serviceClient<suturo_perception_msgs::GetClusters>("GetClusters");
+  suturo_perception_msgs::GetClusters clusterSrv;
+  clusterSrv.request.s = "get";
+  ROS_INFO_STREAM("ServiceClient initialized");
+  // run until service gets shut down
+  while(true)
+  {
+    if (clusterClient.call(clusterSrv))
+    {
+      ROS_INFO("Cluster Service call successful");
+      ROS_INFO("List size: %ld", (long int)clusterSrv.response.perceivedObjs.size() );
+
+      // wait a sec if list is empty. service may not be ready yet
+      if((long int)clusterSrv.response.perceivedObjs.size() == 0)
+      {
+        boost::this_thread::sleep(boost::posix_time::seconds(1));
+        ROS_INFO_STREAM("No objects perceived yet.");
+        continue;
+      }
+
+      for(int i=0; i < clusterSrv.response.perceivedObjs.size(); i++ ) {
+        ROS_INFO("ID of perceived object is: %d", clusterSrv.response.perceivedObjs[i].c_id);
+        ROS_INFO("Volume of perceived object is: %f", clusterSrv.response.perceivedObjs[i].c_volume);
+        ROS_INFO("Centroid(x) of perceived object is: %f , %f , %f ",
+          clusterSrv.response.perceivedObjs[i].c_centroid.x,
+          clusterSrv.response.perceivedObjs[i].c_centroid.y,
+          clusterSrv.response.perceivedObjs[i].c_centroid.z);
+      }
+      ROS_INFO_STREAM("------------------------------------------------------------");
+    }
+    else
+    {
+      ROS_ERROR("Failed to call service GetClusters");
+      return 1;
+    }
+    boost::this_thread::sleep(boost::posix_time::seconds(1));
+  }
+
+  return 0;
+}
