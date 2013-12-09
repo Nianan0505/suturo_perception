95c6fed42b32e800f2d8b0734ee6521b888b1f77
Andre Lübken <aluebken@tzi.de>
2013-12-09 14:54:10 +0100

    PER-44 Add independent logging to suturo_perception_utils

--- suturo_perception_utils/include/suturo_perception_utils.h
+++ suturo_perception_utils/include/suturo_perception_utils.h
@@ -1,8 +1,13 @@
 #ifndef SUTURO_PERCEPTION_UTILS_H
 #define SUTURO_PERCEPTION_UTILS_H
 
+#include <iostream>
 #include <string>
+
+#ifndef HAVE_NO_ROS
 #include "ros/ros.h"
+#endif
+
 
 
 namespace suturo_perception_utils
@@ -10,16 +15,20 @@ namespace suturo_perception_utils
   class SuturoPerceptionUtils
   {
     private:
-      ros::NodeHandle &_node_handle;
+      std::string module;
+      enum level {DEBUG, INFO, WARN, ERROR};
+      void log(level, std::string s);
 
     public:
+      // Constructor to instantiate a logger for a module
+      SuturoPerceptionUtils(std::string moduleName);
       /**
        * log methods for different levels
        */
-      void logDebug  (std::string s);
-      void logInfo   (std::string s);
-      void logWarning(std::string s);
-      void logError  (std::string s);
+      void logDebug(std::string s);
+      void logInfo (std::string s);
+      void logWarn (std::string s);
+      void logError(std::string s);
      
   };
 }
--- suturo_perception_utils/src/suturo_perception_utils.cpp
+++ suturo_perception_utils/src/suturo_perception_utils.cpp
@@ -2,9 +2,65 @@
 
 using namespace suturo_perception_utils;
 
+SuturoPerceptionUtils::SuturoPerceptionUtils(std::string m) : module(m){}
+
+void SuturoPerceptionUtils::log(level lvl, std::string s)
+{ 
+  #ifndef HAVE_NO_ROS
+  switch(lvl)
+  {
+    case DEBUG:
+      ROS_DEBUG("[%s]  %s", module.c_str(), s.c_str());
+    break;
+    case INFO:
+      ROS_INFO( "[%s]  %s", module.c_str(), s.c_str());
+    break;
+    case WARN:
+      ROS_WARN( "[%s]  %s", module.c_str(), s.c_str());
+    break;
+    case ERROR:
+      ROS_ERROR("[%s]  %s", module.c_str(), s.c_str());
+    break;
+  }
+  
+  #else
+    switch(lvl)
+  {
+    case DEBUG:
+      std::cout << "[DEBUG] " << "[" << module << "]  " << s << std::endl;
+    break;
+    case INFO:
+      std::cout << "[INFO] "  << "[" << module << "]  " << s << std::endl;
+    break;
+    case WARN:
+      std::cout << "[WARN] "  << "[" << module << "]  " << s << std::endl;
+    break;
+    case ERROR:
+      std::cout << "[ERROR] " << "[" << module << "]  " << s << std::endl;
+    break;
+  }
+  #endif
+
+}
+
 void SuturoPerceptionUtils::logDebug(std::string s)
 {
-  std::cerr << "test" << std::endl;
+  log(DEBUG, s);
+}
+
+void SuturoPerceptionUtils::logInfo(std::string s)
+{
+  log(INFO, s);
+}
+
+void SuturoPerceptionUtils::logWarn(std::string s)
+{
+  log(WARN, s);
+}
+
+void SuturoPerceptionUtils::logError(std::string s)
+{
+  log(ERROR, s);
 }
 
 // vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2: 
