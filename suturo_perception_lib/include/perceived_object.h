6bdb36d056792d7787892f335e5c11fe6eb7d960
ofenrohr <ofenrohr@tzi.de>
2013-12-13 12:10:39 +0100

    PER-53 #time 10m added average hsv and histogram quality to header and msg

--- suturo_perception_lib/include/perceived_object.h
+++ suturo_perception_lib/include/perceived_object.h
@@ -16,8 +16,12 @@ namespace suturo_perception_lib
       uint8_t c_color_average_r;
       uint8_t c_color_average_g;
       uint8_t c_color_average_b;
+      uint8_t c_color_average_h;
+      uint8_t c_color_average_s;
+      uint8_t c_color_average_v;
       std::string c_recognition_label_2d;
       std::vector<int> c_hue_histogram;
+      uint8_t c_hue_histogram_quality;
       ROI c_roi;
   };
 }
