993b8a1a631bbed48ce1203d1d62b2f3a7690016
Patrick Mania <pmania@tzi.de>
2013-12-14 02:20:24 +0100

    PER-40 Wrote first draft for the 2d label annotator

---
+++ suturo_perception_2d_capabilities/include/suturo_perception_2d_capabilities/label_annotator_2d.h
@@ -0,0 +1,35 @@
+#ifndef SUTURO_PERCEPTION_LABEL_ANNOTATOR_2D_H
+#define SUTURO_PERCEPTION_LABEL_ANNOTATOR_2D_H
+
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include "opencv2/core/core.hpp"
+#include "capability.h"
+#include "perceived_object.h"
+#include "object_matcher.h"  // Include 2d Object recognizer
+#include "suturo_perception_utils.h"
+
+namespace suturo_perception_2d_capabilities
+{
+  /**
+   * This class will use the 
+   * 2d_object_recognition_lib to match a given
+   * image with trained images and put the recognized
+   * label into the given PerceivedObject.
+   *
+   * The ROI for the object recognition has to be passed in the
+   * PerceivedObject.
+   */
+  class LabelAnnotator2D : public suturo_perception_lib::Capability
+  {
+    public:
+      // capability method
+      LabelAnnotator2D(boost::shared_ptr<cv::Mat> original_image, ObjectMatcher &object_matcher);
+      void execute();
+
+    private:
+      boost::shared_ptr<cv::Mat> original_image_;
+      ObjectMatcher &object_matcher_;
+      suturo_perception_utils::Logger logger_;
+  };
+}
+#endif
---
+++ suturo_perception_2d_capabilities/src/label_annotator_2d.cpp
@@ -0,0 +1,28 @@
+#include "suturo_perception_2d_capabilities/label_annotator_2d.h"
+
+using namespace suturo_perception_2d_capabilities;
+
+LabelAnnotator2D::LabelAnnotator2D(boost::shared_ptr<cv::Mat> original_image, ObjectMatcher &object_matcher)
+  : original_image_(original_image), object_matcher_(object_matcher)
+{
+  logger_ = suturo_perception_utils::Logger("LabelAnnotator2D");
+}
+
+void LabelAnnotator2D::execute()
+{
+    suturo_perception_lib::ROI &roi = perceivedObject.c_roi;
+    cv::Rect region_of_interest = cv::Rect(
+        roi.origin.x,
+        roi.origin.y,
+        roi.width,
+        roi.height);
+    cv::Mat image_roi = (*original_image_)(region_of_interest);
+
+    ObjectMatcher::ExecutionResult om_res = object_matcher_.recognizeTrainedImages(image_roi, true); // run headless (true)
+    if(!om_res.label.empty()){
+      logger_.logInfo( (boost::format(" Recognized a object with the label %s") % om_res.label).str() );
+      perceivedObject.c_recognition_label_2d = om_res.label;
+    }else{
+      logger_.logInfo( "No Label found" );
+    }
+}
