5f10a42c39069b6437fce9e4d905e836ae9a51a7
Andre Lübken <aluebken@tzi.de>
2013-12-16 15:43:50 +0100

    make client text fit to crappy CRT

--- suturo_perception_rosnode/src/client.cpp
+++ suturo_perception_rosnode/src/client.cpp
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
                   clusterSrv.response.perceivedObjs[i].c_color_average_g,
                   clusterSrv.response.perceivedObjs[i].c_color_average_b);
         ROS_INFO("2D recognition label of perceived object is: %s", clusterSrv.response.perceivedObjs[i].recognition_label_2d.c_str());
-        ROS_INFO("ROI of perceived object is (origin, width, height): ( %f x %f ) , %i , %i ",
+        ROS_INFO("ROI of object: (origin, width, height): ( %f x %f ) , %i , %i ",
           clusterSrv.response.perceivedObjs[i].c_roi_origin.x,
           clusterSrv.response.perceivedObjs[i].c_roi_origin.y,
           clusterSrv.response.perceivedObjs[i].c_roi_width, 
