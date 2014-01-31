f01a8eb51540611999cd5ed51df3bac07c570c48
Patrick Mania <pmania@lab05.(none)>
2014-01-31 01:24:04 +0100

    PER-71 added testfile, added test todo with rotated cornflakes box

--- suturo_perception_match_cuboid/test/test.cpp
+++ suturo_perception_match_cuboid/test/test.cpp
@@ -389,9 +389,9 @@ TEST(suturo_perception_match_cuboid, match_corny_with_table)
   SUCCEED();
 }
 
+// TODO: Build test
 // Estimated normal of the table: -0.0315049 -0.906697 -0.420605 0.772575
 // cornflakes_on_table
-// Gives wrong pose estimation right now
 
 
 int main(int argc, char **argv) 
