44d47d31e19f21873398ab65237e282e4e806111
Andre Lübken <aluebken@tzi.de>
2014-02-07 17:56:23 +0100

    PER-85 scaffold of barcode rosnode

--- suturo_perception_barcodescanner/src/suturo_perception_barcodescanner_node.cpp
+++ suturo_perception_barcodescanner/src/suturo_perception_barcodescanner_node.cpp
@@ -5,10 +5,71 @@ using namespace zbar;
 
 SuturoPerceptionBarcodeScannerNode::SuturoPerceptionBarcodeScannerNode(ros::NodeHandle& n) : nh_(n)
 {
-  ImageScanner is;
+  logger = Logger("perception_barcodescanner");
+  
+  barcodeService_ = nh_.advertiseService("/suturo/barcodescanner", 
+    &SuturoPerceptionBarcodeScannerNode::getCode, this);
+
+  imageTopic_ = "";
+  processing_ = false;
+
+  logger.logInfo("BarcodeScanner Service ready!");
+
 }
+
+void SuturoPerceptionBarcodeScannerNode::receive_image(const sensor_msgs::ImageConstPtr& inputImage)
+{
+  ImageScanner is;
+
+  
+
+  mutex_.lock();
+  currentBarcode_ = ""; //result from is
+  mutex_.unlock();
+
+  processing_ = false;
+}
+
+bool SuturoPerceptionBarcodeScannerNode::getCode(suturo_perception_msgs::GetBarcode::Request &req,
+        suturo_perception_msgs::GetBarcode::Response &res)
+{
+  logger.logInfo("Got service call");
+  processing_ = true;
+
+  sub_image_ = nh_.subscribe(imageTopic_, 1, 
+          &SuturoPerceptionBarcodeScannerNode::receive_image, this);
+  
+  ros::Rate r(20); // 20 hz
+  boost::posix_time::ptime cancelTime = boost::posix_time::second_clock::local_time() + boost::posix_time::seconds(10);
+  while(processing_)
+  {
+    if(boost::posix_time::second_clock::local_time() >= cancelTime)
+    {
+      processing_ = false;
+      logger.logError("CodeScanner lib didn't return in time. Call failed");
+      return false;
+    }
+      
+    ros::spinOnce();
+    r.sleep();
+  }
+
+  mutex_.lock();
+  res.barcode = currentBarcode_; // set service result
+  mutex_.unlock();
+
+  return true;
+}
+
 
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "suturo_perception_barcodescanner");
+  ros::NodeHandle nh;
+  SuturoPerceptionBarcodeScannerNode bsn(nh);
+  
+  ros::MultiThreadedSpinner spinner(2);
+  spinner.spin();
+  
   return 0;
 }
