4d4224a8d50803b528de39f9885b5d873014a794
Andre Lübken <aluebken@tzi.de>
2014-02-07 17:56:23 +0100

    PER-85 scaffold of barcode rosnode

--- suturo_perception_barcodescanner/include/suturo_perception_barcodescanner_node.h
+++ suturo_perception_barcodescanner/include/suturo_perception_barcodescanner_node.h
@@ -3,9 +3,11 @@
 
 #include "ros/ros.h"
 #include <sensor_msgs/Image.h>
+#include <boost/signals2/mutex.hpp>
 #include <zbar.h>
 
 #include "suturo_perception_utils.h"
+#include "suturo_perception_msgs/GetBarcode.h"
 
 using namespace suturo_perception_utils;
 using namespace zbar;
@@ -18,10 +20,18 @@ namespace suturo_perception_barcodescanner
     public:
       SuturoPerceptionBarcodeScannerNode(ros::NodeHandle& n);
       void receive_image(const sensor_msgs::ImageConstPtr& inputImage);
+      bool getCode(suturo_perception_msgs::GetBarcode::Request &req,
+        suturo_perception_msgs::GetBarcode::Response &res);
 
     private:
       ros::NodeHandle nh_;
       ros::ServiceServer barcodeService_;
+      Logger logger;
+      ros::Subscriber sub_image_;
+      std::string imageTopic_;
+      std::string currentBarcode_;
+      boost::signals2::mutex mutex_;
+      bool processing_;
   };
 
 }
