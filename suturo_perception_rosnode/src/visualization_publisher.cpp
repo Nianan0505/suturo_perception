4370bd8a9bfac31a3ab34df73ad389e5066d4b28
Patrick Mania <pmania@suturo02.(none)>
2014-02-20 20:02:11 +0100

    PER-71 added publisher for cuboid markers

--- suturo_perception_rosnode/src/visualization_publisher.cpp
+++ suturo_perception_rosnode/src/visualization_publisher.cpp
@@ -9,6 +9,7 @@ VisualizationPublisher::VisualizationPublisher(ros::NodeHandle& n, std::string f
 {
   maxMarkerId = 0;
   vis_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 0);  
+  cuboid_pub = nh.advertise<visualization_msgs::Marker>("/suturo/cuboid_markers", 0);  
   logger = Logger("perception_rosnode");
 }
 
@@ -98,4 +99,42 @@ void VisualizationPublisher::publishMarkers(std::vector<suturo_perception_msgs::
     vis_pub.publish(marker);
   }
   maxMarkerId = markerId;
+}
+
+void VisualizationPublisher::publishCuboids(std::vector<suturo_perception_msgs::PerceivedObject> objs)
+{
+  int markerId = 100; // start at id 100 for cuboid markers
+  std::vector<suturo_perception_msgs::PerceivedObject>::iterator it = objs.begin();
+  for (std::vector<suturo_perception_msgs::PerceivedObject>::iterator it = objs.begin(); 
+       it != objs.end (); ++it)
+  {
+    // Publish visualization markers
+    visualization_msgs::Marker cuboidMarker;
+    cuboidMarker.header.frame_id = frameId;
+    cuboidMarker.header.stamp = ros::Time();
+    cuboidMarker.ns = "suturo_perception";
+    cuboidMarker.id = markerId;
+    cuboidMarker.lifetime = ros::Duration(1);
+    cuboidMarker.type = visualization_msgs::Marker::CUBE;
+    cuboidMarker.action = visualization_msgs::Marker::ADD;
+
+    cuboidMarker.pose.position.x = it->matched_cuboid.pose.position.x;
+    cuboidMarker.pose.position.y = it->matched_cuboid.pose.position.y;
+    cuboidMarker.pose.position.z = it->matched_cuboid.pose.position.z;
+    cuboidMarker.pose.orientation.x = it->matched_cuboid.pose.orientation.x;
+    cuboidMarker.pose.orientation.y = it->matched_cuboid.pose.orientation.y;
+    cuboidMarker.pose.orientation.z = it->matched_cuboid.pose.orientation.z;
+    cuboidMarker.pose.orientation.w = it->matched_cuboid.pose.orientation.w;
+    cuboidMarker.scale.x = it->matched_cuboid.length1;
+    cuboidMarker.scale.y = it->matched_cuboid.length2;
+    cuboidMarker.scale.z = it->matched_cuboid.length3;
+    cuboidMarker.color.a = 1.0;
+    cuboidMarker.color.r = 1.0;
+    cuboidMarker.color.g = 0.0;
+    cuboidMarker.color.b = 0.0;
+
+    cuboid_pub.publish(cuboidMarker);
+    markerId++;
+  }
+
 }
