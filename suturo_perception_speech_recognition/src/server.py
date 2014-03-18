fff4c1d6294d2e8e8a5bb12af2609f859e88862e
Andre Lübken <aluebken@tzi.de>
2014-03-18 20:45:51 +0100

    First version of speech recognition server

---
+++ suturo_perception_speech_recognition/src/server.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+
+import rospy
+import suturo_perception_msgs.msg
+import BaseHTTPServer
+import time
+
+''' 
+This server handles incoming requests from the speech recognition 
+android app and returns a status to it. 
+If a valid command was found, it will be sent via ros.
+'''
+
+HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 8000
+COMMAND_WORDS = set(['cleanup', 'remove'])
+OBJECT_WORDS = set(['corny', 'cafetfilter'])
+
+def publish_command(command, object):
+    ''' publish the recognized command '''
+    pub = rospy.Publisher('/suturo/SpeechRecognitionCommand', suturo_perception_msgs.msg.SpeechRecognitionCommand)
+    rospy.init_node('talker', anonymous=True)
+    r = rospy.Rate(10) # 10hz
+    rospy.loginfo('publishing...')
+    s = suturo_perception_msgs.msg.SpeechRecognitionCommand()
+    s.object = 'corny'    #placeholder
+    s.command = 'cleanup' #placeholder
+    pub.publish(s)
+    r.sleep()
+
+class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+  def do_HEAD(s):
+    s.send_response(200)
+    s.send_header("Content-type", "text/plain")
+    s.end_headers()
+  
+  def do_GET(s):
+    """Respond to a GET request."""
+    s.send_response(200)
+    s.send_header("Content-type", "text/plain")
+    s.end_headers()
+    
+    print 'request: ' + s.path
+    request = set(s.path.split('/')[1].split('_'))
+    
+    if request.intersection(COMMAND_WORDS) and request.intersection(OBJECT_WORDS):
+      s.wfile.write("OK")
+    else: s.wfile.write("NOK")
+
+    try:
+      publish_command(request.intersection(COMMAND_WORDS).pop(), 
+                      request.intersection(OBJECT_WORDS).pop()) # use one command and object from the set
+    except rospy.ROSInterruptException: pass
+      
+if __name__ == '__main__':
+  server_class = BaseHTTPServer.HTTPServer
+  httpd = server_class((HOST_NAME, PORT_NUMBER), HttpHandler)
+  print time.asctime(), "Server started - %s:%s" % (HOST_NAME, PORT_NUMBER)
+  try:
+      httpd.serve_forever()
+  except KeyboardInterrupt:
+      pass
+  httpd.server_close()
---
+++ suturo_perception_speech_recognition/src/webs.py
@@ -0,0 +1,31 @@
+import time
+import BaseHTTPServer
+
+
+HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 8000
+
+
+class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/plain")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/plain")
+        s.end_headers()
+        s.wfile.write("OK")
+        print 'command: ' + s.path
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), HttpHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
