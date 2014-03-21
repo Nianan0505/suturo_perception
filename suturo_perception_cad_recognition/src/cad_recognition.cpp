7c75b14fb9a66fa9021146ffc51e39c66f8f25ae
pmania <pmania@pmbun.(none)>
2014-03-21 10:06:45 +0100

    PER-94 added package for cad recognition

---
+++ suturo_perception_cad_recognition/src/cad_recognition.cpp
@@ -0,0 +1,76 @@
+/*
+ * This node tries is part of a pipeline
+ * to align a CAD model to a (partial) pointcloud
+ * of an segmented object
+ *
+ * The CAD model has to be subsampled as a Pointcloud and be
+ * passed to this node.
+ * You can subsample a CAD model with CloudCompare (http://www.danielgm.net/cc/)
+ * Future releases of this software may automate this step.
+ */
+
+#include <ros/ros.h>
+#include <iostream>
+#include <tf/transform_listener.h>
+#include <boost/program_options.hpp>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+
+namespace po = boost::program_options;
+using namespace boost;
+using namespace std;
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "cad_recognition");
+  ros::NodeHandle node;
+  
+  std::string cad_model_pc_filename;
+  std::string input_pc_filename;
+
+  // "HashMap" for program parameters
+  po::variables_map vm;
+  try
+  {
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "produce help message")
+      ("input-pc,i", po::value<std::string>(&input_pc_filename)->required(), "The  filename of the input pointcloud.")
+      ("cad-model-pc,m", po::value<std::string>(&cad_model_pc_filename)->required(), "A pointcloud of the CAD-Model to match. You can get a Pointcloud of your CAD-Model with CloudCompare.")
+    ;
+
+    po::positional_options_description p;
+    po::store(po::command_line_parser(argc, argv).
+    options(desc).positional(p).run(), vm); 
+
+    if (vm.count("help")) {
+      std::cout << "Usage: registration_w_transformations [-t topic]" << endl << endl;
+      std::cout << desc << "\n";
+      return 1;
+    }
+
+    // Put notify after the help check, so help is display even
+    // if required parameters are not given
+    po::notify(vm);
+
+  }
+  catch(std::exception& e)
+  {
+    std::cout << "Usage: cad_recognition -i input_cloud.pcd -m cad_model_cloud.pcd" << endl << endl;
+    std::cerr << "Error: " << e.what() << "\n";
+    return false;
+  }
+  catch(...)
+  {
+    std::cerr << "Unknown error!" << "\n";
+    return false;
+  } 
+
+  // RegistrationWithTransformations r(cloud_topic, frame_id_from, frame_id_to);
+  // Spin
+  ros::spin ();
+
+  return 0;
+};
