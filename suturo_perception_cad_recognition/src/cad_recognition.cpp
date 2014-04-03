020a0f4c44478f14d404c397529643cca7aaefba
Patrick Mania <pmania@tzi.de>
2014-04-04 00:32:39 +0200

    PER-94 moved ransac alignment to a different subclass. increased pointcloud size to get a better initial alignment. Still have to the fix the omnipresent ICP misbehaving that objects are aligned to high or too low.

--- suturo_perception_cad_recognition/src/cad_recognition.cpp
+++ suturo_perception_cad_recognition/src/cad_recognition.cpp
@@ -39,7 +39,7 @@ class InitialAlignment
     pcl::PointCloud<pcl::PointXYZ>::Ptr _cloud_in;
     pcl::PointCloud<pcl::PointXYZ>::Ptr _model_cloud;
 
-    InitialAlignment(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in, pcl::PointCloud<pcl::PointXYZ>::Ptr model_cloud)
+    InitialAlignment(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in, pcl::PointCloud<pcl::PointXYZ>::Ptr model_cloud) : _cloud_in(cloud_in), _model_cloud(model_cloud)
     {
     }
     pcl::PointCloud<pcl::PointXYZ>::Ptr execute(); // Return an initially aligned cloud
@@ -117,6 +117,15 @@ class RANSACInitialAlignment : public InitialAlignment
     }
 };
 
+class TableInitialAlignment : public InitialAlignment
+{
+public:
+  TableInitialAlignment(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in, pcl::PointCloud<pcl::PointXYZ>::Ptr model_cloud) : InitialAlignment(cloud_in,model_cloud)
+  {
+
+  }
+};
+
 
 void estimateNormals(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::Normal>::Ptr output_normals){
   pcl::search::KdTree<pcl::PointXYZ>::Ptr searchMethod(new pcl::search::KdTree<pcl::PointXYZ>);
@@ -154,6 +163,13 @@ void estimateFeaturesAndNormals(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::
 //   fpfh_est.compute(*output_features);
 // 
 // }
+void drawNormalizedVector(pcl::visualization::PCLVisualizer &viewer, Eigen::Vector3f origin, Eigen::Vector3f vector, std::string identifier, int &viewport)
+{
+  pcl::PointXYZ p_origin(origin[0], origin[1], origin[2] );
+  pcl::PointXYZ p_vector(vector[0], vector[1], vector[2] );
+
+  viewer.addLine<pcl::PointXYZ> (p_origin, p_vector, identifier, viewport);
+}
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "cad_recognition");
@@ -221,29 +237,32 @@ int main(int argc, char** argv){
   // Downsample both clouds
   pcl::VoxelGrid<pcl::PointXYZ> sor;
   sor.setInputCloud (input_cloud);
-  #define LEAF_SIZE 0.01f
+  // #define LEAF_SIZE 0.01f
+  #define LEAF_SIZE 0.005f
   sor.setLeafSize (LEAF_SIZE, LEAF_SIZE, LEAF_SIZE);
   sor.filter (*input_cloud_voxeled);
 
   sor.setInputCloud (model_cloud);
   sor.filter (*model_cloud_voxeled);
 
-  // Estimate the normals and features for the initial estimate that will be done later
-  pcl::PointCloud<pcl::Normal>::Ptr normals1(new pcl::PointCloud<pcl::Normal>);
-  pcl::PointCloud<pcl::Normal>::Ptr normals2(new pcl::PointCloud<pcl::Normal>);
-  pcl::PointCloud<EstimationFeature>::Ptr features1(new pcl::PointCloud<EstimationFeature>);
-  pcl::PointCloud<EstimationFeature>::Ptr features2(new pcl::PointCloud<EstimationFeature>);
+  boost::posix_time::ptime file_load_end = boost::posix_time::microsec_clock::local_time();
+  suturo_perception_utils::Logger l("cad_recognition");
+  l.logTime(file_load_start,file_load_end,"File loading and voxeling done");
 
-  estimateFeaturesAndNormals(input_cloud, normals1, features1);
-  estimateFeaturesAndNormals(model_cloud, normals2, features2);
+  boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
+  // Initial Alignment
+  RANSACInitialAlignment ria(input_cloud_voxeled, model_cloud_voxeled);
+  // RANSACInitialAlignment ria(input_cloud, model_cloud);
+  ria.computeFeatures(input_cloud, model_cloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr model_initial_aligned = ria.execute();
 
   input_cloud = input_cloud_voxeled;
   model_cloud = model_cloud_voxeled;
 
-  boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time();
 
   // Compute the centroids of both clouds and bring them closer together
   // for an rough initial alignment.
+  /*
   Eigen::Vector4f input_cloud_centroid, model_cloud_centroid, diff_of_centroids;
   pcl::compute3DCentroid(*input_cloud,input_cloud_centroid); 
   pcl::compute3DCentroid(*model_cloud,model_cloud_centroid); 
@@ -252,26 +271,12 @@ int main(int argc, char** argv){
   Eigen::Affine3f transform = pcl::getTransformation(diff_of_centroids[0],
       diff_of_centroids[1], diff_of_centroids[2],0,0,0);
   pcl::transformPointCloud(*model_cloud, *model_cloud,transform);
+  */
 
-  boost::posix_time::ptime file_load_end = boost::posix_time::microsec_clock::local_time();
-  suturo_perception_utils::Logger l("cad_recognition");
-  l.logTime(file_load_start,file_load_end,"File loading");
+  // TODO
+  // - Use the highest and lowest point to align the Pointcloud better in eight
+  // - Use the table normal to fit the rotation
 
-  // Compute a rough alignment
-  pcl::SampleConsensusInitialAlignment<pcl::PointXYZ, pcl::PointXYZ, EstimationFeature> sac_ia;
-  sac_ia.setMinSampleDistance(0.05f);
-  // sac_ia.setMaxCorrespondenceDistance(0.01f*0.01f);
-  sac_ia.setMaxCorrespondenceDistance(0.03f*0.03f); // works pretty well
-  // sac_ia.setMaxCorrespondenceDistance(0.08f*0.08f);
-  sac_ia.setMaximumIterations(1500);
-
-  sac_ia.setInputCloud(model_cloud);
-  sac_ia.setSourceFeatures(features2);
-  sac_ia.setInputTarget(input_cloud);
-  sac_ia.setTargetFeatures(features1);
-
-  pcl::PointCloud<pcl::PointXYZ>::Ptr model_initial_aligned (new pcl::PointCloud<pcl::PointXYZ>);
-  sac_ia.align(*model_initial_aligned);
 
   // Refine the result with ICP
 
@@ -298,21 +303,23 @@ int main(int argc, char** argv){
 
   pcl::visualization::PCLVisualizer viewer;
   int v1,v2,v3,v4;
-  viewer.createViewPort(0.0,0, 0.25,1, v1 );
-  viewer.addText("Input Cloud", 0.1, 0.1 , "input_cloud_text_id", v1 );
-  viewer.createViewPort(0.25,0, 0.5,1, v2 );
+  // viewer.createViewPort(0.0,0, 0.25,1, v1 );
+  // viewer.addText("Input Cloud", 0.1, 0.1 , "input_cloud_text_id", v1 );
+  viewer.createViewPort(0.0,0, 0.33,1, v2 );
   viewer.addText("Model vs. Input Cloud", 0.1, 0.1 , "model_cloud_text_id", v2 );
-  viewer.createViewPort(0.5,0, 0.75  ,1, v3 );
+  viewer.createViewPort(0.33,0, 0.66  ,1, v3 );
   viewer.addText("Initial Aligned Cloud", 0.1, 0.1 , "initial_aligned_cloud_text_id", v3 );
-  viewer.createViewPort(0.75,0, 1  ,1, v4 );
+  viewer.createViewPort(0.66,0, 1  ,1, v4 );
   viewer.addText("Refined Aligned Cloud", 0.1, 0.1 , "refined_aligned_cloud_text_id", v4 );
 
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> green_color(input_cloud, 0, 255, 0);
-  viewer.addPointCloud<pcl::PointXYZ> (input_cloud, green_color, "input_cloud_id",v1);
+  // pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> green_color(input_cloud, 0, 255, 0);
+  // viewer.addPointCloud<pcl::PointXYZ> (input_cloud, green_color, "input_cloud_id",v1);
 
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> red_color(model_cloud, 255, 0, 0);
   viewer.addPointCloud<pcl::PointXYZ> (input_cloud, green_color, "input_cloud_id2",v2);
   viewer.addPointCloud<pcl::PointXYZ> (model_cloud, red_color, "model_cloud_id",v2);
+  drawNormalizedVector(viewer, Eigen::Vector3f(0,0,0), Eigen::Vector3f(0,0,1), "norm_at_origin", v2);
+  viewer.addCoordinateSystem(0.5,v2);
 
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> red_color2(model_initial_aligned, 255, 0, 0);
   viewer.addPointCloud<pcl::PointXYZ> (model_initial_aligned, red_color2,"initial_aligned_cloud_id",v3);
@@ -332,3 +339,30 @@ int main(int argc, char** argv){
 
   return 0;
 };
+  // Estimate the normals and features for the initial estimate that will be done later
+  // pcl::PointCloud<pcl::Normal>::Ptr normals1(new pcl::PointCloud<pcl::Normal>);
+  // pcl::PointCloud<pcl::Normal>::Ptr normals2(new pcl::PointCloud<pcl::Normal>);
+  // pcl::PointCloud<EstimationFeature>::Ptr features1(new pcl::PointCloud<EstimationFeature>);
+  // pcl::PointCloud<EstimationFeature>::Ptr features2(new pcl::PointCloud<EstimationFeature>);
+
+  // estimateFeaturesAndNormals(input_cloud, normals1, features1);
+  // estimateFeaturesAndNormals(model_cloud, normals2, features2);
+
+  // Compute a rough alignment
+  /*
+  pcl::SampleConsensusInitialAlignment<pcl::PointXYZ, pcl::PointXYZ, EstimationFeature> sac_ia;
+  sac_ia.setMinSampleDistance(0.05f);
+  // sac_ia.setMaxCorrespondenceDistance(0.01f*0.01f);
+  sac_ia.setMaxCorrespondenceDistance(0.03f*0.03f); // works pretty well
+  // sac_ia.setMaxCorrespondenceDistance(0.08f*0.08f);
+  sac_ia.setMaximumIterations(1500);
+
+  sac_ia.setInputCloud(model_cloud);
+  sac_ia.setSourceFeatures(features2);
+  sac_ia.setInputTarget(input_cloud);
+  sac_ia.setTargetFeatures(features1);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr model_initial_aligned (new pcl::PointCloud<pcl::PointXYZ>);
+  sac_ia.align(*model_initial_aligned);
+  */
+
